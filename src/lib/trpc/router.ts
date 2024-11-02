690c0eaa62b144b266d269e6dbc3646f88790d1e
adimar <adimaraisha@gmail.com>
2024-11-02 19:39:36 +0800

    added trpc

---
+++ src/lib/trpc/router.ts
@@ -0,0 +1,15 @@
+import { initTRPC } from '@trpc/server';
+import type { Context } from './context';
+
+export const t = initTRPC.context<Context>().create();
+
+export const router = t.router({
+	greeting: t.procedure.query(async () => {
+		// await delay(500); // 👈 simulate an expensive operation
+		return `Hello tRPC v10 @ ${new Date().toLocaleTimeString()}`;
+	})
+});
+
+export const createCaller = t.createCallerFactory(router);
+
+export type Router = typeof router;
